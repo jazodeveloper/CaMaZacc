ea24304a49ecee61642fee70446d932b7a40ebf3
Jaziel Isai Valdez <jazoo@192.168.1.14>
2025-11-22 02:13:58 -0600

    initial

--- server/db.ts
+++ server/db.ts
@@ -5,11 +5,15 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL must be set. Did you forget to provision a database?");
+  throw new Error("No existe DATABASE_URL");
 }
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    // 'require' is not a valid ConnectionOptions property; keep supported TLS options
+    rejectUnauthorized: false   // <--- EVITA EL ERROR DE CERTIFICADO
+  }
 });
 
 export const db = drizzle(pool, { schema });
